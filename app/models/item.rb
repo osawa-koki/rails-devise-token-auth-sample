3cac20446fd6d24d35c36f59f59702ee89c4e647
osawa-koki <github@osawa.dev>
2023-07-22 17:18:51 +0900

    モデルの依存関係を書いた。

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,6 @@
 # frozen_string_literal: true
 
 class Item < ApplicationRecord
+  has_many :prefecture_items, dependent: :destroy
+  has_many :prefectures, through: :prefecture_items
 end
--- app/models/prefecture_item.rb
+++ app/models/prefecture_item.rb
@@ -1,4 +1,6 @@
 # frozen_string_literal: true
 
 class PrefectureItem < ApplicationRecord
+  belongs_to :prefecture
+  belongs_to :item
 end
