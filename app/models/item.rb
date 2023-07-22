2923f3fc0b9ec0dcac4c628212259b947bd3887a
osawa-koki <github@osawa.dev>
2023-07-22 18:41:34 +0900

    不要なモデルを削除した。

--- app/models/item.rb
+++
@@ -1,6 +0,0 @@
-# frozen_string_literal: true
-
-class Item < ApplicationRecord
-  has_many :prefecture_items, dependent: :destroy
-  has_many :prefectures, through: :prefecture_items
-end
--- app/models/prefecture_item.rb
+++
@@ -1,6 +0,0 @@
-# frozen_string_literal: true
-
-class PrefectureItem < ApplicationRecord
-  belongs_to :prefecture
-  belongs_to :item
-end
